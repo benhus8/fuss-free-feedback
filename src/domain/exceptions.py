b9aaf77f14b0a2b15fda57b83ea6aafb0885ccb1
benhus8 <benedykt.huszcza@gmail.com>
2026-01-06 00:52:07 +0100

    refactor exceptions.py to improve formatting and organization of error classes

--- src/domain/exceptions.py
+++ src/domain/exceptions.py
@@ -1,21 +1,35 @@
 # TODO implement RFC 7807
 class DomainError(Exception):
     """Base exception for all domain-related errors."""
+
     pass
+
 
 class InboxExpiredError(DomainError):
     """Raised when an action is attempted on an expired inbox."""
+
     pass
+
 
 class TopicChangeNotAllowedError(DomainError):
     """Raised when attempting to change the topic of an inbox that already has messages."""
+
     pass
+
 
 class AnonymousMessagesNotAllowedError(DomainError):
     """Raised when an anonymous message is sent to an inbox that requires signatures."""
+
     pass
 
-## TODO change to not found for security reasons?
+
+class NotFoundError(DomainError):
+    """Raised when the specified resource does not exist."""
+
+    pass
+
+
 class InvalidSignatureError(DomainError):
     """Raised when the provided credentials do not match the inbox owner."""
+
     pass
