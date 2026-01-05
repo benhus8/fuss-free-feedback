936d71c82089d1a37ad0177b74b5f55f8a8c5964
benhus8 <benedykt.huszcza@gmail.com>
2026-01-06 00:53:02 +0100

    style: improve formatting and organization in InboxMapper class

--- src/infrastructure/mappers/inbox_mapper.py
+++ src/infrastructure/mappers/inbox_mapper.py
@@ -1,7 +1,9 @@
 from src.domain.models import Inbox, Message
 from src.infrastructure.database.models import InboxDB, MessageDB
+from datetime import timezone
 
-#TODO unit tests of that mapper
+
+# TODO unit tests of that mapper
 class InboxMapper:
     """
     Data Mapper Pattern.
@@ -11,23 +13,25 @@ class InboxMapper:
     def to_domain(self, db_entity: InboxDB) -> Inbox:
         if not db_entity:
             return None
-            
+
+        expires_at = db_entity.expires_at
+        if expires_at.tzinfo is None:
+            expires_at = expires_at.replace(tzinfo=timezone.utc)
+
         domain_messages = [
             Message(
-                id=m.id,
-                body=m.body,
-                created_at=m.created_at,
-                signature=m.signature
-            ) for m in db_entity.messages
+                id=m.id, body=m.body, created_at=m.created_at, signature=m.signature
+            )
+            for m in db_entity.messages
         ]
 
         return Inbox(
             id=db_entity.id,
             topic=db_entity.topic,
             owner_signature=db_entity.owner_signature,
-            expires_at=db_entity.expires_at,
+            expires_at=expires_at,
             allow_anonymous=db_entity.allow_anonymous,
-            messages=domain_messages
+            messages=domain_messages,
         )
 
     def to_db(self, domain_entity: Inbox) -> InboxDB:
@@ -37,8 +41,9 @@ class InboxMapper:
                 body=m.body,
                 created_at=m.created_at,
                 signature=m.signature,
-                inbox_id=domain_entity.id
-            ) for m in domain_entity.messages
+                inbox_id=domain_entity.id,
+            )
+            for m in domain_entity.messages
         ]
 
         return InboxDB(
@@ -47,5 +52,5 @@ class InboxMapper:
             owner_signature=domain_entity.owner_signature,
             expires_at=domain_entity.expires_at,
             allow_anonymous=domain_entity.allow_anonymous,
-            messages=messages_db
+            messages=messages_db,
         )
