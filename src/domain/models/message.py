45e9a291fb59eb27454073a12b5136c29883e7d4
benhus8 <benedykt.huszcza@gmail.com>
2026-01-06 00:52:43 +0100

    style: improve formatting of created_at attribute in Message class

--- src/domain/models/message.py
+++ src/domain/models/message.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
+
 @dataclass
 class Message:
     """
     Represents a single message within an Inbox.
     """
+
     body: str
-    created_at: datetime # in UTC format
+    created_at: datetime  # in UTC format
     signature: Optional[str] = None
     id: Optional[int] = None
