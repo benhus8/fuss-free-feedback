eceb6e49d1ba01f494a3df4181ef82d65636556e
benhus8 <benedykt.huszcza@gmail.com>
2026-01-06 19:17:58 +0100

    add inbox id to message model

--- src/domain/models/message.py
+++ src/domain/models/message.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
+import uuid
 
 
 @dataclass
@@ -9,6 +10,7 @@ class Message:
     Represents a single message within an Inbox.
     """
 
+    inbox_id: uuid.UUID
     body: str
     created_at: datetime  # in UTC format
     signature: Optional[str] = None
